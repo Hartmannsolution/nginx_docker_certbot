7890db60e80245807faae7563eba3304dcd33b25
Thomas-Hartmann <thomas@webtrade.dk>
2021-03-20 18:38:40 +0100

    updated init script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -35,7 +35,7 @@ docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
+    -subj '/CN=cphdat.dk'" certbot
 echo
 
 
